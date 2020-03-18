de741f2bb42d51bcf586647f89186a6d4b76307b
charleseduardome <charleseduardo.me@gmail.com>
2020-03-18 17:17:59 -0300

    Autenticação

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,9 +1,18 @@
+import producer from 'immer';
+
 const INITIAL_STATE = {
-  token: 123,
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return producer(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
