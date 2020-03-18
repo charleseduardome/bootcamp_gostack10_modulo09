2b3aea3a59aa11feec6171e267669375d4ca0311
charleseduardome <charleseduardo.me@gmail.com>
2020-03-18 20:19:17 -0300

    Persistindo autenticação

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 
@@ -7,17 +8,19 @@ import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
 
-import store from './store'; // sempre vir depois da importação do reactotron
+import { store, persistor } from './store'; // sempre vir depois da importação do reactotron
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
