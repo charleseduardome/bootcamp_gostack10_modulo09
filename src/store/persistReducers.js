9ee4bf3e43d7fce29abe0a13c3ba9e8b6bb34a80
charleseduardome <charleseduardo.me@gmail.com>
2020-03-18 21:04:27 -0300

    Loading da autenticação

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default reducers => {
   const persistReducers = persistReducer(
     {
-      key: 'root',
+      key: 'gobarber',
       storage,
       whitelist: ['auth', 'user'],
     },
