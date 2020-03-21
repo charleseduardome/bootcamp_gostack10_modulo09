ef6ea1c2cd02940bbec08cb838c92fc10caf2c65
charleseduardome <charleseduardo.me@gmail.com>
2020-03-21 15:50:25 -0300

    Logout da aplicação

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
+import { signOut } from '~/store/modules/auth/actions';
+
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -15,6 +17,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -41,7 +47,9 @@ export default function Profile() {
         <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button">Sair do Gobarber</button>
+      <button onClick={handleSignOut} type="button">
+        Sair do Gobarber
+      </button>
     </Container>
   );
 }
