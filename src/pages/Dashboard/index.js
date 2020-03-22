5590ef75bb299082c616445281d27624b56ef809
charleseduardome <charleseduardo.me@gmail.com>
2020-03-21 23:16:49 -0300

    Listando agendamentos

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,60 +1,95 @@
-import React, { useState, useMemo } from 'react';
-
-import { format, subDays, addDays } from 'date-fns';
+import React, { useState, useMemo, useEffect } from 'react';
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  setMilliseconds,
+  isBefore,
+  isEqual,
+  parseISO,
+} from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 import pt from 'date-fns/locale/pt';
-
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
 export default function Dashboard() {
+  const [schedule, setSchedule] = useState([]);
   const [date, setDate] = useState(new Date());
 
   const dateFormatted = useMemo(
-    () => format(date, "d 'de' MMMM", { locale: pt }),
+    () => format(date, "d 'de ' MMMM", { locale: pt }),
     [date]
   );
 
-  function hadlePrevDay() {
+  useEffect(() => {
+    async function loadSchedule() {
+      const response = await api.get('schedule', {
+        params: { date },
+      });
+
+      const { appointments } = response.data;
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setMilliseconds(
+          setSeconds(setMinutes(setHours(date, hour), 0), 0),
+          0
+        );
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: appointments.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          ),
+        };
+      });
+
+      setSchedule(data);
+    }
+
+    loadSchedule();
+  }, [date]);
+
+  function handlePrevDay() {
     setDate(subDays(date, 1));
   }
-  function hadleNextDay() {
+
+  function handleNextDay() {
     setDate(addDays(date, 1));
   }
 
   return (
     <Container>
       <header>
-        <button type="button" onClick={hadlePrevDay}>
+        <button type="button" onClick={handlePrevDay}>
           <MdChevronLeft size={36} color="#FFF" />
         </button>
         <strong>{dateFormatted}</strong>
-        <button type="button" onClick={hadleNextDay}>
+        <button type="button" onClick={handleNextDay}>
           <MdChevronRight size={36} color="#FFF" />
         </button>
       </header>
 
       <ul>
-        <Time past>
-          <strong>08:00</strong>
-          <span>Charles Eduardo</span>
-        </Time>
-
-        <Time available>
-          <strong>09:00</strong>
-          <span>Charles</span>
-        </Time>
-
-        <Time>
-          <strong>10:00</strong>
-          <span>Charles Eduardo</span>
-        </Time>
-
-        <Time>
-          <strong>11:00</strong>
-          <span>Charles Eduardo</span>
-        </Time>
+        {schedule.map(time => (
+          <Time key={time.time} past={time.past} available={!time.appointment}>
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : 'Em aberto'}
+            </span>
+          </Time>
+        ))}
       </ul>
     </Container>
   );
